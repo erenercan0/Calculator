21b27aab4aec94b7e6922786824976c2cda2e0b0
Eren Emin Ercan <47062220+erenercan0@users.noreply.github.com>
2019-01-27 14:31:11 -0600

    Add multiply method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,7 @@ public static double add(double num1 , double num2){
 public static double subtract(double num1, double num2){
     return num1-num2;
 }
+    public static double multiply(double num1, double num2){
+        return num1*num2;
+    }
 }
