b5ba5e52c2e76de63dba6e925a6a6d681cbc5ddf
Eren Emin Ercan <47062220+erenercan0@users.noreply.github.com>
2019-01-27 22:02:22 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,10 @@ public static double subtract(double num1, double num2){
     public static double divide(double n1, double n2){
     double result = n1 /n2;
         return result;
+        //this is a comment in GITHUB 1
      public static double divide(double num1, double num2){
         return num1/num2;
     }
+        
+        //this is a comment in GITHUB 2
 }
