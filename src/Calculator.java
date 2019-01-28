80055b6c8627ae6d45b921aad28a280ca34e4310
Erenercan <erenercan0@gmail.com>
2019-01-27 22:05:47 -0600

    add local comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,22 @@
 public class Calculator {
-public static double add(double num1 , double num2){
-    return num1 + num2;
-}
-//another operator
-public static double subtract(double num1, double num2){
-    return num1-num2;
-}
-    public static double multiply(double num1, double num2){
-        return num1*num2;
+    public static double add(double num1, double num2) {
+        return num1 + num2;
     }
-    public static double divide(double n1, double n2){
-    double result = n1 /n2;
+
+    //another operator
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    //LOCAL REPO: Comment 1
+    public static double divide(double n1, double n2) {
+        double result = n1 / n2;
         return result;
-     public static double divide(double num1, double num2){
-        return num1/num2;
     }
-}
+        //LOCAL REPO: Comment 1
+    }
+
