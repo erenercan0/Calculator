55ada37a437f4f3546bab345ec22c02d42c5e2ef
Eren Emin Ercan <47062220+erenercan0@users.noreply.github.com>
2019-01-27 20:03:33 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 public static double add(double num1 , double num2){
     return num1 + num2;
 }
-
+//another operator
 public static double subtract(double num1, double num2){
     return num1-num2;
 }
