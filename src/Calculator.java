90ed9fad8f37d4169a506e417dd58140e2de79a4
Eren Emin Ercan <47062220+erenercan0@users.noreply.github.com>
2019-01-27 20:21:09 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,4 +9,7 @@ public static double subtract(double num1, double num2){
     public static double multiply(double num1, double num2){
         return num1*num2;
     }
+     public static double divide(double num1, double num2){
+        return num1/num2;
+    }
 }
